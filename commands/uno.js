aeee8739ab9c16d3cfb9efc0a2f0b6847f191abf
Sapphire7x <36426445+justinsapphire@users.noreply.github.com>
2022-08-07 00:05:05 -0700

    Attempt at getting reactors

--- commands/uno.js
+++ commands/uno.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
+const { SlashCommandBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle, ComponentType } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -10,19 +10,55 @@ module.exports = {
             fetchReply: true 
         });
 		message.react('✅').catch(error => console.error('Failed to react with checkmark', error));
-        
+        const messageId = message.id;
+
         const row = new ActionRowBuilder()
             .addComponents(
                 new ButtonBuilder()
-                    .setCustomId('startUno')
+                    .setCustomId(`startUno: ${messageId}`)
                     .setLabel('Start')
                     .setStyle(ButtonStyle.Success),
             );
 
-        await interaction.followUp({
+        const hostMessage = await interaction.followUp({
             content: 'Click **Start** to start this game!',
             ephemeral: true,
-            components: [row]
+            components: [row],
+            fetchReply: true
         })
+
+        const collector = hostMessage.createMessageComponentCollector({ componentType: ComponentType.Button, time: 15000 });
+        let startCheck = false;
+
+        collector.on('collect', i => {
+            // Start is clicked
+
+            //Game cannot start cases:
+
+            //Game is starting cases:
+            startCheck = true;
+            await i.update({
+                content: 'The Game has Started!',
+                ephemeral: true,
+                components: []
+            });
+            let peopleInGame = [];
+            const userReactions = message.reactions.cache.filter(reaction => !reaction.users.cache.has(client.user.id));
+            console.log(userReactions);
+        });
+
+        collector.on('end', collected => {
+            if(!startCheck) {
+                //Timed out
+                await i.update({
+                    content: 'The game has timed out.',
+                    ephemeral: true,
+                    components: []
+                })
+                await interaction.editReply('The game has timed out.')
+            }
+        });
+
+
 	},
 };
