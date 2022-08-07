b013479abdfeed67d63d065bd7004323db20c20a
Sapphire7x <36426445+justinsapphire@users.noreply.github.com>
2022-08-06 18:07:08 -0700

    Event Handler

--- index.js
+++ index.js
@@ -19,6 +19,20 @@ for (const file of commandFiles) {
 	client.commands.set(command.data.name, command);
 }
 
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+	const filePath = path.join(eventsPath, file);
+	const event = require(filePath);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
+
+/*
 // When the client is ready, run this code (only once)
 client.once('ready', () => {
 	console.log('Ready!');
@@ -28,29 +42,14 @@ client.on('interactionCreate', async interaction => {
 	if (!interaction.isChatInputCommand()) return;
 
 	const command = client.commands.get(interaction.commandName);
-
 	if (!command) return;
-
 	try {
 		await command.execute(interaction);
 	} catch (error) {
 		console.error(error);
 		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
 	}
-
-	/*const { commandName } = interaction;
-
-	if (commandName === 'ping') {
-		await interaction.reply('Pong!');
-	} else if (commandName === 'server') {
-		await interaction.reply(`Server name: ${interaction.guild.name}
-		\nTotal members: ${interaction.guild.memberCount}
-		\nCreated at: ${interaction.guild.createdAt}`);
-	} else if (commandName === 'user') {
-		await interaction.reply(`Your tag: ${interaction.user.tag}
-		\nYour id: ${interaction.user.id}`);
-	}*/
-});
+});*/
 
 // Login to Discord with your client's token
 client.login(token);
