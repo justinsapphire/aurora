5d8927fd642fd7950fad7290261ba3ceee7b3d7f
justinsapphire <36426445+justinsapphire@users.noreply.github.com>
2022-08-06 18:27:59 -0700

    Defined Client

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -2,14 +2,14 @@ module.exports = {
 	name: 'interactionCreate',
 	async execute(interaction) {
 		if (!interaction.isChatInputCommand()) return;
-
-        const command = client.commands.get(interaction.commandName);
-        if (!command) return;
-        try {
-            await command.execute(interaction);
-        } catch (error) {
-            console.error(error);
-            await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-        }
+		const client = interaction.client
+		const command = client.commands.get(interaction.commandName);
+		if (!command) return;
+		try {
+		    await command.execute(interaction);
+		} catch (error) {
+		    console.error(error);
+		    await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+		}
 	},
 };
