13445ce8a173330e2281f4a81398ea218f51021b
Sapphire7x <36426445+justinsapphire@users.noreply.github.com>
2022-08-13 20:09:50 -0700

    trying to fix this error on create table

--- events/ready.js
+++ events/ready.js
@@ -8,14 +8,22 @@ module.exports = {
 	name: 'ready',
 	once: true,
 	execute(client) {
-		;(async () => {
+		/*;(async () => {
             await pool.query('CREATE TABLE IF NOT EXISTS games (channelid INT(255) NOT NULL, gamedata VARCHAR(max) NOT NULL, PRIMARY KEY (channelid))')
         })().catch(err =>
             setImmediate(() => {
 				console.log("error");
                 throw err
             })
-        )
+        )*/
+        pool.connect( (err, client, done) => {
+            client.query('create table if not exists games( \
+                channelid INT(255) primary key, \
+                gamedata VARCHAR(MAX))', (err, result) => {
+                    //disconnent from database on error
+                    done(err);
+            });
+        });
 		console.log(`Ready! Logged in as ${client.user.tag}`);
 	},
 };
