a534188c63ac31d2dd676f6ad622716be5d8660a
Sapphire7x <36426445+justinsapphire@users.noreply.github.com>
2022-08-15 00:06:30 -0700

    okay back to what it was for ready

--- events/ready.js
+++ events/ready.js
@@ -3,15 +3,17 @@ let pool = require('../pool.js');
 module.exports = {
 	name: 'ready',
 	once: true,
-	async execute(client) {
-        let clients = await pool.connect()
-        let result = await clients.query({
-            rowMode: 'array',
-            text: 'CREATE TABLE IF NOT EXISTS GAMES (channelid INT(255) primary key, gamedata VARCHAR(MAX))'
-        })
-        console.log(result ? result.rows : "no result") // [ [ 1, 2 ] ]
-        await clients.end()
-
+	execute(client) {
+        pool.connect( (err, clients, done) => {
+            clients.connection.on('message', console.log)
+            clients.query('CREATE TABLE IF NOT EXISTS GAMES ( \
+                channelid INT(255) primary key, \
+                gamedata VARCHAR(MAX))', (err, result) => {
+                    console.log(result ? result : "no result?");
+                    //disconnent from database on error
+                    done(err);
+            });
+        });
 		console.log(`Ready! Logged in as ${client.user.tag}`);
 	},
 };
